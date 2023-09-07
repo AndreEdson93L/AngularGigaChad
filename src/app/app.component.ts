32a996bfb1f5e9cfdacbbe41996d2940aff449bf
Lorenzoni Andrea Edson <a.lorenzoni@crif.com>
2023-09-07 22:24:29 +0200

    Writing my personal notes about angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
 
+//Decorator @Component( etc .. approccio dichiarativo )
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  //templateUrl: `<h1>Not Cool But Possible</h1>`,
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
+  //Componenti del modulo (visibili da app-component)
   declarations: [
     AppComponent
   ],
+  //Import dei moduli
+  //Se vogliamo rendere visibile un componente presente in un altro modulo
+  //Possiamo importare qui il modulo.
   imports: [
     BrowserModule,
     AppRoutingModule
@@ -16,3 +20,42 @@ import { AppComponent } from './app.component';
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+/*
+
+// Module_A {
+  Components:
+  - Component_A1
+  - Component_A2
+  - Component_A3
+  
+  Imports:
+  // No other modules imported
+
+  Exports:
+  // If you want these components to be available outside of Module_A, they should be exported.
+}
+
+// Module_B {
+  Components:
+  - Component_B1
+  - Component_B2
+  - Component_B3
+
+  Imports:
+  // No other modules imported
+
+  Exports:
+  // If you want these components to be available outside of Module_B, they should be exported.
+}
+
+// AppModule (Main Module) {
+  Components:
+  - AppComponent (usually the root component)
+  
+  Imports:
+  - Module_A (importing the entire Module_A)
+  - Module_B (importing the entire Module_B)
+}
+
+*/
