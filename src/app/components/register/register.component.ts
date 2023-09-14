fcb80d9d6d89780639e838143d189bf0181624fe
Lorenzoni Andrea Edson <a.lorenzoni@crif.com>
2023-09-14 12:32:34 +0200

    Fixed bug in register.component.ts

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { User } from 'src/app/models/user.model';
 import { UserService } from 'src/app/services/user-service';
 import { NgForm } from '@angular/forms';
@@ -8,19 +8,26 @@ import { NgForm } from '@angular/forms';
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
 
   user : User = {
     username: '',
     password: '',
     email: ''
   }
+
+  users: User[] = []
  
   constructor(private userService: UserService){}
 
-  onSubmit(registrationForm : NgForm){
-
-    this.userService.registerUser(this.user)
-    registrationForm.reset()
+  onSubmit(registrationForm: NgForm) {
+    this.userService.registerUser({ ...this.user });  // Send a new instance to the service
+    this.users = this.userService.getUsers();
+    registrationForm.reset();
+  }
+  
+  //get users
+  ngOnInit(){
+    this.users = this.userService.getUsers()
   }
 }
--- src/app/services/user-service.ts
+++ src/app/services/user-service.ts
@@ -13,23 +13,22 @@ export class UserService {
   // 2 . constructor
   constructor() { }
 
-  registerUser(user: User){
-   const existingUser = this.users.find(u => u.email.toLowerCase() === u.email.toLowerCase())
-    if(existingUser){
+  registerUser(user: User) {
+    const existingUser = this.users.find(u => u.email.toLowerCase() === user.email.toLowerCase());
+    if (existingUser) {
       console.log('Email already registered');
       return;
     }
-
-   this.users.push(user)
   
-   console.log({
-    "username" : user.username,
-    "password" : user.password,
-    "email" : user.email
-  })
-
+    this.users = [...this.users, { ...user }];  // Use spread operator to push a new instance
+    
+    console.log({
+      "username": user.username,
+      "password": user.password,
+      "email": user.email
+    });
   }
-
+  
   getUsers(): User[] {
     return this.users;
   }
